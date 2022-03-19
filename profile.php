fe21e8e734e8270828281b1d5758fdcd4f86f9c5
pandaking321 <48794264+pandaking321@users.noreply.github.com>
2022-03-19 10:37:30 +0800

    Add profile pic uploading function
    For database,
    1. Create a new column called f_path with varchar(255)
    1. Create a new column called f_name with varchar(255)
    
    For file management,
    1. Create a new folder called image outside the UniSched-main folder and store the uploadImage.jpg image (which is uploaded on Github) inside it
    2. Create a new folder called profile pic inside the UniSched-main folder for storing the profile pictures

--- profile.php
+++ profile.php
@@ -1,5 +1,4 @@
 <?php
-require __DIR__.'/lib/db.inc.php';
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
@@ -7,14 +6,21 @@ if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit;
 }
-global $db;
-$db = unisched_DB();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'login_account';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $db->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT password, email, f_path FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $email);
+$stmt->bind_result($password, $email, $f_path);
 $stmt->fetch();
 $stmt->close();
 ?>
@@ -26,7 +32,15 @@ $stmt->close();
     <title>Profile Page</title>
     <link href="css/home.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
+    <meta charset=utf-8 />
 </head>
+<style>
+  article, aside, figure, footer, header, hgroup, 
+  menu, nav, section { display: block; }
+</style>
 <body class="loggedin">
 <nav class="navtop">
     <div>
@@ -44,6 +58,57 @@ $stmt->close();
         <p>Your account details are below:</p>
         <table>
             <tr>
+                <td>Profile Pic:</td>
+                <form action="profile.php" method="post" enctype="multipart/form-data">
+                    <td><label>
+                    <?php	
+                        if ($f_path == NULL) {
+                            $f_path = "/image/uploadimage.jpg";
+                        }
+                    ?> 
+                    <img id="output" src="<?php echo $f_path ?>" width="50%" height="50%" class="image"/>
+                    <br><input name="Filename" type="file" accept="image/*" onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])" style="display:none;">
+                    <br><input TYPE="submit" name="upload" value="Submit"/></label></td>
+                </form>
+                <?php	
+                    // Upload the profile pic 
+                    if(isset($_POST['upload'])) {
+                        // We need to use sessions, so you should always start sessions using the below code.
+                        session_start();
+                        $id = $_SESSION['id'];
+
+                        $fileExistsFlag = 0; 
+                        $fileName = $_FILES['Filename']['name'];
+                        $DATABASE_HOST = 'localhost';
+                        $DATABASE_USER = 'root';
+                        $DATABASE_PASS = '';
+                        $DATABASE_NAME = 'login_account';
+                        $link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+                        $target = "profile pic/";		
+                        $fileTarget = $target.$fileName;	
+                        $tempFileName = $_FILES["Filename"]["tmp_name"];
+                        $result = move_uploaded_file($tempFileName,$fileTarget);
+
+                        if($result) { 
+                            $fileTarget = "/UniSched-main/";	
+                            $fileTarget .= $target.$fileName;	
+                            echo '<script>alert("Your new profile picture has been successfully uploaded!")</script>';		
+                            $query = "UPDATE accounts SET f_path = '$fileTarget', f_name = '$fileName' WHERE id = $id";;
+                            $link->query($query) or die("Error : ".mysqli_error($link));	
+                        }
+                        else {		
+                            echo '<script>alert("Please upload a new profile picture!")</script>';		
+                        }
+                        mysqli_close($link);
+                        // Refresh the page to show the new profile pic
+                        $page = $_SERVER['PHP_SELF'];
+                        echo '<meta http-equiv="Refresh" content="0;' . $page . '">';
+                        
+                    } 
+                ?> 
+            </tr>
+            <tr>
                 <td>Username:</td>
                 <td><?=$_SESSION['name']?></td>
             </tr>
@@ -58,7 +123,6 @@ $stmt->close();
         </table>
     </div>
 </div>
-
-
 </body>
+
 </html>
