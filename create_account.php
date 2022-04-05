79cf6a65c5440f808590dd3edf8c41d32cc62a34
JoshA21 <joshatwal21@gmail.com>
2022-04-05 14:38:51 +0800

    Logged in check removed from create account

--- create_account.php
+++ create_account.php
@@ -2,11 +2,7 @@
 require __DIR__.'/lib/db.inc.php';
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-    header('Location: index.html');
-    exit;
-}
+
 global $db;
 $db = unisched_DB();
 
