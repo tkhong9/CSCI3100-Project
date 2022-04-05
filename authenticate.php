8856cefe6dca25ccc5db5b4b2547877340cb07ef
pandaking321 <48794264+pandaking321@users.noreply.github.com>
2022-04-05 19:16:40 +0800

    Add code for checking verified account

--- authenticate.php
+++ authenticate.php
@@ -13,40 +13,54 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $db->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $db->prepare('SELECT id, password, isAdmin, verified FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
 	if ($stmt->num_rows > 0) {
-    	$stmt->bind_result($id, $password);
+    	$stmt->bind_result($id, $password, $isAdmin, $verified);
     	$stmt->fetch();
     	// Account exists, now we verify the password.
     	// Note: remember to use password_hash in your registration file to store the hashed passwords.
     	if (password_verify($_POST['password'], $password)) {
-    		// Verification success! User has logged-in!
-    		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-    		session_regenerate_id();
-    		$_SESSION['loggedin'] = TRUE;
-    		$_SESSION['name'] = $_POST['username'];
-    		$_SESSION['id'] = $id;
-            header('Location: home.php');
+         if ($verified === 1) {
+   		      // Verification success! User has logged-in!
+        		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+        		session_regenerate_id();
+        		$_SESSION['loggedin'] = TRUE;
+        		$_SESSION['name'] = $_POST['username'];
+        		$_SESSION['id'] = $id;
+        		if($isAdmin == 1){
+                		header('Location: adminHome.php');
+        		}else{
+        		    header('Location: home.php');
+        		} 
+         } else {
+       		    // Unverified account
+              $message = "Account has not been verified!";
+        		  echo "<script>
+        		  alert('$message');
+        		  window.location.href='index.html';
+        		  </script>";    
+         }
+
     	} else {
     		// Incorrect password
             $message = "Incorrect username and/or password!";
-			echo "<script>
-			alert('$message');
-			window.location.href='index.html';
-			</script>";
+        		echo "<script>
+        		alert('$message');
+        		window.location.href='index.html';
+        		</script>";
     	}
     } else {
     	// Incorrect username
-		$message = "Incorrect username and/or password!";
-		echo "<script>
-		alert('$message');
-		window.location.href='index.html';
-		</script>";
+        $message = "Incorrect username and/or password!";
+    		echo "<script>
+    		alert('$message');
+    		window.location.href='index.html';
+    		</script>";
     }
 
 
