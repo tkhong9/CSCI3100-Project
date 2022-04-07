f79f54684d3348ee8b76d39c7f532b230a614255
pandaking321 <48794264+pandaking321@users.noreply.github.com>
2022-04-07 16:41:59 +0800

    Edit for the way of database connection

--- reset_username.php
+++ reset_username.php
@@ -1,22 +1,19 @@
 <?php
+require __DIR__.'/lib/db.inc.php';
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
+
+global $db;
+$db = unisched_DB();
+
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit;
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'unisched';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email, f_path FROM accounts WHERE id = ?');
+$stmt = $db->prepare('SELECT password, email, f_path FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
@@ -72,7 +69,7 @@ $stmt->close();
                     $username = $_POST['new_username'];
 
                     $stmt = "UPDATE accounts SET username = '$username' WHERE id = $id";
-                    if($con->query($stmt) === TRUE){
+                    if($db->query($stmt) === TRUE){
                          $message = "Your username is changed!";	
                            
                          if ($_POST['new_username'] === $_SESSION['name']){
