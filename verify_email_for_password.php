2bd1955b95ef11a82aac66ad20c330e52b09b812
pandaking321 <48794264+pandaking321@users.noreply.github.com>
2022-04-07 16:42:47 +0800

    Edit for the way of database connection

--- verify_email_for_password.php
+++ verify_email_for_password.php
@@ -5,21 +5,17 @@
 
     <body>
     <?php
+        require __DIR__.'/lib/db.inc.php';
+        
+        global $db;
+        $db = unisched_DB();
+    
         if($_GET['key'] && $_GET['token'])
         { 
             $email = $_GET['key'];
             $token = $_GET['token'];
-
-            $DATABASE_HOST = 'localhost';
-            $DATABASE_USER = 'root';
-            $DATABASE_PASS = '';
-            $DATABASE_NAME = 'unisched';
-            $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-            if (mysqli_connect_errno()) {
-                exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-            }
         
-            $stmt = $con->prepare('SELECT buffer_password FROM accounts WHERE token =? and email =?');
+            $stmt = $db->prepare('SELECT buffer_password FROM accounts WHERE token =? and email =?');
             // In this case we can use the account ID to get the account info.
             $stmt->bind_param('ss', $token, $email);
             $stmt->execute();
@@ -29,9 +25,9 @@
 
             if ($buffer_password != NULL){
                 $sql = "UPDATE accounts SET buffer_password = NULL WHERE token = '$token' AND email = '$email'";
-                mysqli_query($con, $sql);
+                mysqli_query($db, $sql);
                 $sql = "UPDATE accounts SET password = '$buffer_password' WHERE token = '$token' AND email = '$email'";
-                mysqli_query($con, $sql);
+                mysqli_query($db, $sql);
                 $message = "Your new password has been verified.";
             }else{
                 $message = "ERROR";
